12832a2963c9651c9efda45cfe9a7dc4cc19445b
Huy Phan Ba <pbhuy@aavn.local>
2018-04-16 15:13:06 +0700

    first commit

---
+++ src/main/java/com/axonactive/CRUDDemo/App.java
@@ -0,0 +1,13 @@
+package com.axonactive.CRUDDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
